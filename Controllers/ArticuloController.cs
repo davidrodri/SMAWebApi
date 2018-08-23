4c41b1ba505263df692186c892f8b26ce647c915
David Rodriguez <david_rodriguezsuarez@hotmail.com>
2018-08-22 22:40:35 -0600

    Initial commit

---
+++ Controllers/ArticuloController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SMAWebApi.Models;
+
+namespace SMAWebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Articulo")]
+    public class ArticuloController : Controller
+    {
+        private DataContext db = new DataContext();
+
+        [HttpGet("ObtenerTodos")]
+        public IActionResult ObtenerProductos()
+        {
+            var articulo = db.Articulo.ToList();
+            if (articulo == null)
+            {
+                return NotFound();
+            }
+            return Ok(articulo);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult ObtenerPorCodigoBarra(string id)
+        {
+            var articulo = db.Articulo.FirstOrDefault(c => c.CodigoBarra == id);
+            if (articulo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(articulo);
+
+        }
+    }
+}
---
+++ Models/Articulo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SMAWebApi.Models
+{
+    public class Articulo
+    {
+        public string ArticuloID { get; set; }
+        public string NombreArticulo { get; set; }
+        public string CodigoBarra { get; set; }
+        //public double PrecioCosto { get; set; }
+        //public double PrecioVenta { get; set; }
+    }
+}
